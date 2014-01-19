ee14e36c89a91bed1e6aa10cb595423d877a0658
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-20 02:17:38 +0700

    tests for multiple and modInt

--- test/index.js
+++ test/index.js
@@ -1,6 +1,4 @@
-
 var big = require('../')
-
 var tape = require('tape')
 
 function equal(t, a, b) {
@@ -11,9 +9,9 @@ tape('ADD - add two buffers', function (t) {
   var a = new Buffer([0x01, 0x02, 0x03])
   var b = new Buffer([0x04, 0x04, 0x06])
   var c = new Buffer([0x05, 0x06, 0x09])
-  equal(t, c, big.add(a, b))
-  equal(t, a, big.subtract(c, b))
-  equal(t, b, big.subtract(c, a))
+  equal(t, big.add(a, b),      c)
+  equal(t, big.subtract(c, b), a)
+  equal(t, big.subtract(c, a), b)
   t.end()
 })
 
@@ -21,9 +19,9 @@ tape('ADD - add with with carry', function (t) {
   var a = new Buffer([0xf0, 0x00, 0x00])
   var b = new Buffer([0x13, 0x07, 0x00])
   var c = new Buffer([0x03, 0x08, 0x00])
-  equal(t, c, big.add(a, b))
-  equal(t, a, big.subtract(c, b))
-//  equal(t, b, big.subtract(c, a))
+  equal(t, big.add(a, b),      c)
+  equal(t, big.subtract(c, b), a)
+  equal(t, big.subtract(c, a), b)
   t.end()
 })
 
@@ -43,19 +41,68 @@ tape('ADD - carry over', function (t) {
   t.end()
 })
 
-
-tape('ADD - pass in mutant buffer', function (t) {
+tape('multiply', function (t) {
   var a = new Buffer([0x01, 0x02, 0x03])
-  var b = new Buffer([0x04, 0x04, 0x06])
-  var c = big.add(a, b)
-  equal(t, c, new Buffer([0x05, 0x06, 0x09]))
+  var b = new Buffer([0x02, 0x00, 0x00])
+  var c = new Buffer([0x02, 0x04, 0x06])
+  equal(t, big.mul(a, b), c)
   t.end()
 })
 
-tape('SUB - subtract one buffer from another', function (t) {
-  var a = new Buffer([0x01, 0x02, 0x03])
-  var b = new Buffer([0x04, 0x04, 0x06])
-  var c = new Buffer([0x05, 0x06, 0x09])
-  equal(t, b, big.sub(c, a))
+tape('multiply - carry', function (t) {
+  var a = new Buffer([0x10, 0x00])
+  var b = new Buffer([0x10, 0x00])
+  var c = new Buffer([0x00, 0x01])
+  equal(t, big.mul(a, b), c)
   t.end()
 })
+
+tape('multiply - carry2', function (t) {
+  var a = new Buffer([0x00, 0x10])
+  var b = new Buffer([0x00, 0x10])
+  var c = new Buffer([0x00, 0x00, 0x00, 0x01])
+  equal(t, big.mul(a, b, new Buffer(4)), c)
+  t.end()
+})
+
+tape('multiply - carry3', function (t) {
+  var a = new Buffer([0x10, 0x10])
+  var b = new Buffer([0x10, 0x10])
+  var c = new Buffer([0x00, 0x01, 0x02, 0x01])
+  equal(t, big.mul(a, b, new Buffer(4)), c)
+  t.end()
+})
+
+tape('multiply - carry3', function (t) {
+  var a = new Buffer([0x12, 0x34])
+  var b = new Buffer([0x56, 0x78])
+  var c = new Buffer([0x0c, 0xee, 0x79, 0x18])
+  equal(t, big.mul(a, b, new Buffer(4)), c)
+  t.end()
+})
+
+
+tape('modInt', function (t) {
+  var a = new Buffer([0, 0, 0x12, 0x34])
+  equal(t, big.modInt(a, 0x13), new Buffer([0x11, 0, 0, 0]))
+  t.end()
+})
+
+tape('modInt2', function (t) {
+  var a = new Buffer([0, 0, 0, 0, 0x12, 0x34])
+  equal(t, big.modInt(a, 0x130000), new Buffer([0, 0, 0x11, 0, 0, 0]))
+  t.end()
+})
+
+tape('modInt3', function (t) {
+  var a = new Buffer([0x12, 0x34, 0x56, 0x78, 0x9a, 0, 0, 0])
+  equal(t, big.modInt(a, 0x123456), new Buffer([0xea, 0x47, 0x08, 0, 0, 0, 0, 0]))
+  t.end()
+})
+
+tape('modInt4', function (t) {
+  var a = new Buffer([0xab, 0x33, 0, 0])
+  equal(t, big.modInt(a, 28), new Buffer([11, 0, 0, 0]))
+  t.end()
+})
+ 
