f33e64089040dce4c7c90b14ae44381e93b53f99
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-21 03:20:39 +0700

    why do we bother teaching kids long division and times tables when all they need to understand is bitwise operations?

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 //iterate over buffers from lsb to msb
 //and add each byte & 0xff, carrying overflow.
 
-exports.add = function (a, b, c) {
+
+//okay, need addShift(a, b, n)
+//a + (b<<n)
+var add = exports.add = function (a, b, c) {
   var l = Math.max(a.length, b.length)
   if(!c) { c = new Buffer(l); c.fill() }
   var carry = 0
@@ -27,8 +30,7 @@ exports.subtract = function (a, b, c) {
   for(var i = 0; i < l; i++) {
     var A = (a[i] || 0), B = (b[i] || 0) + carry
     if(A < B) {
-      A += 256
-      carry = 1
+      A += 256; carry = 1
     } else
       carry = 0
     c[i] = A - B
@@ -38,7 +40,8 @@ exports.subtract = function (a, b, c) {
 
 //mulitply.
 //multiply each pair of places and accumulate results in a new buffer.
-exports.mul =
+var multiply = exports.mul =
+
 exports.multiply = function (a, b, c) {
   var l = Math.max(a.length, b.length)
   if(!c) c = new Buffer(l)
@@ -50,10 +53,10 @@ exports.multiply = function (a, b, c) {
   for(var i = 0; i < b.length; i++) {
     for(var j = 0; j < a.length; j++) {
       var value = b[i] * a[j]
-      var current = (c[i + j]||0) + ((c[i + j + 1]<<8) || 0)
+      var current = (c[i + j]||0) | ((c[i + j + 1]<<8) || 0)
       var v = current + value
       c[i + j]     = v & 0xff
-      c[1 + i + j] = v >> 8
+      c[1 + i + j] = v >>> 8
     }
   }
   return c
@@ -62,25 +65,17 @@ exports.multiply = function (a, b, c) {
 // a % n (where n is < 2^31)
 // this uses Buffer#readInt32LE
 // so, buffers length must be multiple of 4.
-exports.modInt = function (a, n, c) {
-  var l = a.length
-  if(!c) c = new Buffer(l)
-  a.copy(c)
-//  l = l - 4
-//  do {
-//    var n = c[l]
-//    c.writeInt32LE(c.readInt32LE(l) % n, l)
-//  } while(l--)
+// should also be able to adapt this to divide by int,
+// which would be useful for converting to different bases (like base 10, or 58)
 
-  var w = 0
-  while(l--) {
-    w = (w << 8 | c[l]) % n
-  }
-
+exports.modInt = function (a, n) {
+  var l = a.length, w = 0
+  while(l--) w = (w << 8 | a[l]) % n
   return w
 }
 
 //return -1, 0, or 1 if a < b, a == b, a > b
+
 var compare = exports.compare = function (a, b) {
   var l = Math.max(a.length, b.length)
   while(l--) {
@@ -101,31 +96,44 @@ var shift = exports.shift = function (a, n, c) {
   var carry = 0
   if(n > 0) {
     for(var i = a.length - 1; i >= -bytes; i--) {
-//      console.log(bytes, bits)
-//      console.log(c, i)
       var v = a[i] << bits
       c[i + bytes + 1] = carry | v >> 8
       carry = c[i + bytes] = v & 0xff
     }
-      
   } else {
     var bits = -1*bits
     for(var i = bytes; i <= a.length; i++) {
       // shift into the second byte,
       // so that we have a chance to grab the carry.
       var v = (a[i] << 8) >> bits
-
-//      console.log('shifted->', new Buffer([v & 0xff, v >> 8]))
-//      console.log('c-byte', a[i], a[i] >> bits)
-//      console.log('t-byte', v >> 8)
-//      console.log('b-byte', v & 0xff)
-//     console.log(c)
       c[i + bytes - 1] = v & 0xff | carry
       carry = c[i + bytes] = v >> 8
-//      carry = v >> 8
     }
   }
   return c
+}
+
+//multiply, using shift + add
+multiply =
+exports.mul =
+exports.multiply = function (a, b, m) {
+  var w = new Buffer(b)
+  var l = a.length * 8, prev = 0
+  if(!m) m = new Buffer(a.length)
+
+  m.fill() //m must be zeros, or it will affect stuff.
+
+  for(var i = 0; i < l; i++) {
+    var byte = i >> 3, bit = i % 8
+
+    //if this bit is 1
+    if(a[byte] & (1 << bit)) {
+      shift(w, i - prev, w)
+      prev = i
+      m = add(m, w)
+    }
+  }
+  return m
 }
 
 function _msb (x) {
@@ -166,23 +174,28 @@ exports.divide = function (a, b, q, r) {
   //shift b to line up with a.
   shift(b, mA - mB, _b)
 
-//  console.log('*** shifted! ******************')
-//  console.log(mA, mB, mA - mB)
-//  console.log(a)
-//  console.log('->', b , '>>', mA - mB, '=', _b)
-
-  while(compare(_b, b) >= 0) {
+  // instead of shifting this along 1 bit at a time
+  // it would probably be more performant to find the
+  // next most significant bit and move that far.
+  // at least in sparse numbers.
+  var bit = mA - mB
+  while(bit >= 0) {
     if(compare(_b, r) <= 0) {
       subtract(r, _b, _r)
-//      console.log('div', _b, '-', r, '=', _r)
       var t = r; r = _r; _r = t
-      //set bit of quotent!    
+      //set 1 bit of quotent!
+      q[bit>>3] |= 1<<(bit%8)
     }
-//    else
-//      console.log('keep', r)
+
     shift(_b, -1, _b)
-//    var t = _b2; _b2 = _b; _b = t
+    bit--
   }
-  return {remainder: r}
+  return {remainder: r, quotient: q}
 }
 
+// to base:
+// b.unshift(_a = (a % 10)); a = (a - _a) / 10; b
+
+exports.square = function (a, m) {
+  return multiply(a, a, m)
+}
