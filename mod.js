f151dd6e7f0080c520c3880dd95dea1cef7fb694
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-20 10:13:39 +0700

    can divide while modding

--- mod.js
+++ mod.js
@@ -1,27 +1,31 @@
-
+ 
 
 // calculate A % B
 function mod(A, B) {
-  var C
+  var C = 1, D = 0
   var _B = B
   if (B > A) return A
 
   //shift B right until it it's just smaller than A.
-  while(B < A) {
-    B<<=1
+ while(B < A) {
+    B<<=1; C<<=1
   }
   //now, shift B back, while subtracting.
   do {
-    B>>=1
-    console.log('%', A.toString(2), B.toString(2))
-    A = B >= A ? A : A - B
-    console.log('=', A.toString(2))
-  } while(B > _B) 
+    B>>=1; C>>=1
 
+    //mark the bits where you could subtract.
+    //this becomes the quotent!
+    if(B < A) {
+      D |= C; A = A - B
+    }
+    console.log('=', D.toString(2))
+  } while(B > _B) 
+  console.log(D, D.toString(2))
   return A
 }
 
-console.log(mod(13227, 28), 13227 % 28)
+console.log(mod(13227, 28), 13227 % 28, ~~(13227 / 28))
 
 //find the most significant bit in a byte with a ternary based binary search...
 function msb (x) {
@@ -31,8 +35,9 @@ function msb (x) {
     : x & 0x0C ? x & 0x08 ? 4 : 3 : x & 0x02 ? 2 : x
   )
 }
-var l = 255
-while(l--) {
-  console.log(msb(l), l.toString(2), l.toString(2).length)
-}
 
+//var l = 255
+//while(l--) {
+//  console.log(msb(l), l.toString(2), l.toString(2).length)
+//}
+//
