eb3806e59fb50fdc5b985c4623ceb6c72a44903d
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-24 20:19:27 +0700

    re-enable power test

--- test/power.js
+++ test/power.js
@@ -3,8 +3,9 @@ var u = require('./util')
 var data = require('./fixtures/pow.json')
 var big = require('../')
 
-//skip this test on browserify,
-//because it just takes too long, and makes the test fail.
-if(process.title !== 'browser')
-  u.createTestMethod('power', data, big.power)
+//have made significant improvements power() perf
+//by truncating remainder buffer
+//(which means that now there arn't a bunch of zeros to iterate over)
+
+u.createTestMethod('power', data, big.power)
 
