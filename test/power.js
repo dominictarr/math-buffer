b05631a6702505cdf013ae24fb1d25737a4691e5
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-24 10:28:55 +0700

    skip power test in browser

--- test/power.js
+++ test/power.js
@@ -3,6 +3,11 @@ var u = require('./util')
 var data = require('./fixtures/pow.json')
 var big = require('../')
 
+//skip this test on browserify,
+//because it just takes too long, and makes the test fail.
+if(process.title === 'browser')
+  return
+
 u.createTestMethod('power', data, big.power)
 //, function (t, actual, expected, name, i) {
 //  u.equal(t, actual.remainder, expected.remainder, 'divide - remainder - ' + i)
