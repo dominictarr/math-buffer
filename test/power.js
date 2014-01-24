a444137971843590031b14863bdd69860d00d143
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-24 10:39:18 +0700

    get esprima to shutup about illegal return statement

--- test/power.js
+++ test/power.js
@@ -5,11 +5,6 @@ var big = require('../')
 
 //skip this test on browserify,
 //because it just takes too long, and makes the test fail.
-if(process.title === 'browser')
-  return
+if(process.title !== 'browser')
+  u.createTestMethod('power', data, big.power)
 
-u.createTestMethod('power', data, big.power)
-//, function (t, actual, expected, name, i) {
-//  u.equal(t, actual.remainder, expected.remainder, 'divide - remainder - ' + i)
-//  u.equal(t, actual.quotient, expected.quotient, 'divide - quotient - ' + i)
-//})
